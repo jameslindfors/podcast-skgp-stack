afed83da81d06336c794dddedbadf9b88a6fd963
jameslindfors <jameslindfors@outlook.com>
2022-10-10 22:13:04 -0700

    frontend login page

--- client/src/routes/auth/+page.svelte
+++ client/src/routes/auth/+page.svelte
@@ -1,13 +1,116 @@
 <script land="ts">
 	import { enhance } from '$app/forms';
+
+	import InstagramIcon from '$lib/assets/icons8-instagram-50.svelte';
+	import SpotifyIcon from '$lib/assets/icons8-spotify-50.svelte';
+	import TwitchIcon from '$lib/assets/icons8-twitch-50.svelte';
+
+	import sendicon from '$lib/assets/icons8-send-50.png';
+
+	/**
+	 * @type {any[]}
+	 */
+	let providers = [
+		{
+			provider: 'instagram',
+			name: 'Instagram',
+			icon: InstagramIcon,
+			color: '#C13584',
+			auth_url: 'https://project-water-circuit.live/auth/instagram'
+		},
+		{
+			provider: 'spotify',
+			name: 'Spotify',
+			icon: SpotifyIcon,
+			color: '#1DB954',
+			auth_url: 'https://project-water-circuit.live/auth/spotify'
+		},
+		{
+			provider: 'twitch',
+			name: 'Twitch',
+			icon: TwitchIcon,
+			color: '#9146FF',
+			auth_url: 'https://project-water-circuit.live/auth/twitch'
+		}
+	];
+	let auth = { email: '' };
 </script>
 
-<form method="POST" action="?/login" use:enhance>
-	<label for="email">Username: </label>
-	<input type="email" name="email" id="email" />
-	<button type="submit">Login</button>
-</form>
-<a href=" https://project-water-circuit.live/auth/twitch">Login With Twitch</a>
-<a href=" https://project-water-circuit.live/auth/spotify">Login With Spotify</a>
-<a href=" https://project-water-circuit.liveauth/twitter">Login With Twitter</a>
-<a href=" https://project-water-circuit.live/auth/instagram">Login With Instagram</a>
+<svelte:head>
+	<title>Login</title>
+	<meta name="description" content="Authentication page" />
+</svelte:head>
+
+<section>
+	<div class="is-flex is-flex-direction-column m-4">
+		<h1 class="has-text-weight-semibold has-text-centered is-size-3">Welcome, login here.</h1>
+		{#each providers as Provider}
+			<a
+				href={Provider.auth_url}
+				class="button is-medium provider"
+				style={`background-color: ${Provider.color}`}
+			>
+				<span class="is-flex is-align-items-center ">
+					<Provider.icon />
+					<span class="pl-2 has-weight-bold">Login with {Provider.name}</span>
+				</span>
+			</a>
+		{/each}
+		<span class="divider" />
+		<form method="POST" action="?/login" use:enhance>
+			<span class="form-row">
+				<span class="field is-large">
+					<label for="email" hidden />
+					<input
+						type="email"
+						name="email"
+						id="email"
+						placeholder="email@email.com"
+						bind:value={auth.email}
+					/>
+				</span>
+				<button type="submit">
+					<img src={sendicon} alt="send" />
+				</button>
+			</span>
+		</form>
+	</div>
+</section>
+
+<style>
+	section {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+	}
+	.divider {
+		border-bottom: 2px solid #ccc;
+		margin: 1rem 0;
+	}
+	.provider {
+		border-radius: 0.2rem;
+		color: #eee;
+		font-weight: 500;
+		margin: 0.3rem 0;
+	}
+	.form-row {
+		display: flex;
+		align-items: center;
+	}
+	.form-row > button {
+		position: relative;
+		z-index: 1;
+		height: 100%;
+		background: none;
+	}
+	.form-row > .field {
+		flex: 1;
+	}
+	.form-row > .field > input {
+		padding: 1.2rem;
+		width: 100%;
+		border: 1px solid #ccc;
+		font-size: large;
+	}
+</style>
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,5 +1,4 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { VitePWA } from 'vite-plugin-pwa';
 import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
@@ -18,24 +17,7 @@ const config: UserConfig = {
 	optimizeDeps: {
 		exclude: ['@urql/svelte']
 	},
-	plugins: [
-		sveltekit(),
-		VitePWA({
-			devOptions: {
-				enabled: true
-			},
-			registerType: 'autoUpdate',
-			injectRegister: 'auto',
-			includeAssets: ['favicon.png'],
-			manifest: {
-				name: 'My Awesome App',
-				short_name: 'MyApp',
-				description: 'My Awesome App description',
-				orientation: 'portrait',
-				display: 'standalone'
-			}
-		})
-	]
+	plugins: [sveltekit()]
 };
 
 export default config;
